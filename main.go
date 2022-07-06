b0f74ca95813b082fa8d22979d7656dd59a6d795
Luiz Alberto <luiz.albuquerque99@gmail.com>
2022-07-06 13:37:12 -0300

    Bug fixing

--- main.go
+++ main.go
@@ -1,17 +1,11 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
 	"github.com/luizAlbuquerque0/go-api-rest/database"
 	"github.com/luizAlbuquerque0/go-api-rest/routes"
 )
 
-func ExibeTodosAlunos(c *gin.Context) {
-	c.JSON(200, gin.H{
-		"id":   "1",
-		"nome": "Luiz",
-	})
-}
+
 
 func main() {
 	database.ConectaComBancoDeDados()
