ddb02bf438b45e5c130260180297638b9b6a4521
Luiz Alberto <luiz.albuquerque99@gmail.com>
2022-07-06 13:31:19 -0300

    .

---
+++ routes/routes.go
@@ -0,0 +1,18 @@
+package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/luizAlbuquerque0/go-api-rest/controllers"
+)
+
+func HandleRequests() {
+	r := gin.Default()
+	r.GET("/alunos", controllers.ExibeTodosAlunos)
+	r.GET("/:nome", controllers.Saldacoa)
+	r.POST("/alunos", controllers.CriaNovoAluno)
+	r.GET("/alunos/:id", controllers.BuscaAlunoPorId)
+	r.DELETE("/alunos/:id", controllers.DeletaAluno)
+	r.PATCH("/alunos/:id", controllers.EditaAluno)
+	r.GET("/alunos/cpf/:cpf", controllers.BuscaAlunoPorCPF)
+	r.Run()
+}
